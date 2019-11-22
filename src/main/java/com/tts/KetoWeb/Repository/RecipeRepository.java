dfc0975a428ded673261245ebf1e9943e75e292c
phibalrath <52164530+phibalrath@users.noreply.github.com>
2019-11-22 16:01:32 -0500

    added findByCategory method

--- src/main/java/com/tts/KetoWeb/Repository/RecipeRepository.java
+++ src/main/java/com/tts/KetoWeb/Repository/RecipeRepository.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Repository;
 
 import com.tts.KetoWeb.Model.Recipe;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long>{
 	
 	Recipe getById(Long id);
+	
+	//Filter By Category
+	List<Recipe> findByCategory(String category);
 }
